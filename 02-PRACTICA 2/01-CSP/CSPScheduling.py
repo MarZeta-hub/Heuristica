a805c5d03066c234a76363fde289fa3b410ff7ba
alejandro <100383181@alumnos.uc3m>
2020-11-26 15:58:39 +0100

    Comienzo practica 2

---
+++ 02-PRACTICA 2/01-CSP/CSPScheduling.py
@@ -0,0 +1,21 @@
+# Heuristica y Optimizacion. Practica 2. Parte 1: Asignacion de antenas de transmision a satelites   
+
+from constraint import *
+
+problem = Problem()
+
+#  Variables a introducir en el problema
+
+
+
+
+# Restricciones
+
+
+
+
+
+
+# Ejecucion del problema
+
+
