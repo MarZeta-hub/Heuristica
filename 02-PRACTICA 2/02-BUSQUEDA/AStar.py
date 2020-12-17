973ef9767d52525429b8cd8259ddac2e97e3bfc4
alejandro <100383181@alumnos.uc3m>
2020-12-17 19:17:28 +0100

    Auto stash before merge of "main" and "origin/main"

--- 02-PRACTICA 2/02-BUSQUEDA/AStar.py
+++ 02-PRACTICA 2/02-BUSQUEDA/AStar.py
@@ -1,3 +1,25 @@
 # Clase AStar para la practica 2 parte 2
 
- from problema import problema
+f = open('problema.prob')
+
+# Obtenemos los observadores
+f.seek(5)
+OBS =f.readline().split(';')
+
+# Objetos es la lista de listas de numeros enteros que se corresponden con las coordenadas de los objetos visibles
+objetos = []
+for i in OBS:
+    lista = []
+    a = i.replace(')','').replace('(','').split(',')
+    lista.append(int(a[0]))
+    lista.append(int(a[1]))
+
+    objetos.append(lista)
+
+print(objetos)
+
+# Obtencion de las caracteristicas de cada uno de los satelites
+
+# Lista donde se van a guardar las caracteristicas de los satelites
+satelites = []
+
